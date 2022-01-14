d341a93c1d364c073514ba824c75dfc0db761c44
Technamic <technamic@protonmail.com>
2022-01-14 17:54:24 -0300

    Alterações feitas no dia 14/01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule} from '.shared/material/material.module';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   imports: [
